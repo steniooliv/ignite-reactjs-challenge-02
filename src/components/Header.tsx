9c2a015ec5d24469d9e86899f01ae4e802930cb3
Stenio Oliveira <steniooliv@gmail.com>
2021-06-10 22:22:48 -0300

    trnasfered header to a new component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { useMovies } from "../hooks/useMovies";
+
+export function Header(){
+  const {selectedGenre} = useMovies();
+  
+  return (
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+  );
+}
