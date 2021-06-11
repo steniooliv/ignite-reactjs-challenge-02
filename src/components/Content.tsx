fe5f978b7541ae0a234de0f3a076fa78fbbc8fbd
Stenio Oliveira <steniooliv@gmail.com>
2021-06-10 22:22:36 -0300

    transfered content to component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
+import { useMovies } from "../hooks/useMovies";
+import { Header } from "./Header";
+import { MovieCard } from "./MovieCard";
+
 export function Content() {
-  // Complete aqui
+
+  const {movies} = useMovies();
+
+  return (
+    <div className="container">
+      <Header />
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+      
+    </div>
+  );
 }
